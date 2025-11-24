11a9a1f794c6512c15d6f8b68dc2153e90df8cad
putrifathonah <putrifathonah_2327240139@mhs.mdp.ac.id>
2025-11-24 15:00:04 +0700

    update : Implementasi Backend dan Intgritas API

--- griya-mdp-backend/app.js
+++ griya-mdp-backend/app.js
@@ -1,48 +1,49 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-//load mongodb db connection
-require('./app_server/models/db');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./app_server/routes/index');
-var housingRouter = require('./app_server/routes/housing');
+// Load MongoDB connection
+require("./app_server/models/db");
+
+var indexRouter = require("./app_server/routes/index");
+var housingRouter = require("./app_server/routes/housing");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'app_server', 'views'));
-app.set('view engine', 'ejs');
+// View engine setup
+app.set("views", path.join(__dirname, "app_server", "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+// Middleware
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
+// CORS Configuration
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all domains
+  res.setHeader("Access-Control-Allow-Origin", "*");
   next();
 });
 
-app.use('/', indexRouter);
-app.use('/housing', housingRouter);
+// Routes
+app.use("/", indexRouter);
+app.use("/housing", housingRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// 404 Handler
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+// Error Handler
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
